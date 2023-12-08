125c29e347d018d6f75789deab815a9d398ebd1d
WemaGoodness <goodnesswema20@gmail.com>
2023-12-08 23:47:40 +0300

    Simple Shell

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+
+/**
+ * main - execute basic UNIX commands
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+	char *lineptr;
+	size_t *n = 0;
+	int get_val, exec_val;
+
+	if (argc < 1)
+	{
+		perror("Usage");
+	}
+
+
+	while (1)
+	{
+		write(1, "$ ", 2);
+		get_val = getline(&lineptr, n, stdin);
+
+		if (get_val == -1)
+		{
+			perror("Error:");
+			exit(98);
+		}
+
+		exec_val = execve(argv[0], argv, envp);
+
+		if (exec_val == -1)
+		{
+			perror("Error:");
+			exit(98);
+		}
+		printf("%s\n", lineptr);
+		free(lineptr);
+	}
+
+	return (0);
+
+}
---
+++ shell.h
@@ -0,0 +1,12 @@
+#define SHELL_H
+#ifndef SHELL_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#endif
